cd046415c19bbd7c4072bd2c81f71e415ec598f5
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-04-18 20:31:50 -0300

    criação- loop/setInterval/ keydoen

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
   mario.classList.add('jump');
@@ -8,4 +9,19 @@ const jump = () => {
   },500);
 }
 
-document.addEventListener('keydow', jump);
+const lopp = setInterval(() => {
+
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+if (pipePosition <= 37 && pipePosition > 0 && marioPosition < 80){
+
+  pipe.style.animation = 'none';
+  pipe.style.left = `${pipePosition}px`;
+
+  mario.style.animation = 'none';
+  mario.style.bottom = `${marioPosition}px`;
+}
+}, 10);
+
+document.addEventListener('keydown', jump);
