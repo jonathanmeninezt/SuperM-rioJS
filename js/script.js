6a2c54bb0a9cb6a1f20a20b00ddc00f50eef6ff5
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-05-01 18:35:43 -0300

    criação- keydow/ArrowUp/Space - jump- JS

--- js/script.js
+++ js/script.js
@@ -13,26 +13,6 @@ const temaMario = new Audio();
 temaMario.src = 'audios/temaMario.wav'
 
 
-// var pontos = 0;
-// var tempo = 0;
-// var intervalo;
-
-// function iniciarJogo() {
-//   intervalo = setInterval(() => {
-//     tempo++;
-//     document.getElementById("tempo").textContent = `Tempo de jogo: ${tempo} segundos`;
-//   }, 1000);
-// }
-
-// function pararJogo() {
-//   clearInterval(intervalo);
-//   document.getElementById("tempo").textContent = `Tempo de jogo: ${tempo} segundos (final)`;
-// }
-
-// function adicionarPontos() {
-//   pontos++;
-//   document.getElementById("pontos").textContent = `Pontos: ${pontos}`;
-// }
 
 const jump = () => {
   mario.classList.add('jump');
@@ -114,4 +94,8 @@ if (blockPosition <= 80 && blockPosition > 0 && marioPosition < 80){
 }
 }, 10);
 
-document.addEventListener('keydown', jump);
+document.addEventListener('keydown', (e) => {
+  if ((e.code === "ArrowUp") | (e.code === "Space")){
+    jump();
+  }
+});
