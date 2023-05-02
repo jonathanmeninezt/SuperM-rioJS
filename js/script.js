0ec64fde64da5f445653edfc8ef8262a2ddbac09
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-05-01 21:40:22 -0300

    criação - score/count - fimJogo/textGameOver - JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+const gameBoard = document.querySelector('.game-board');
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const tartu = document.querySelector('.tartu');
 const block = document.querySelector('.block');
+const score = document.querySelector('#score');
+
+let count = 0;
 
 const somHit = new Audio();
 somHit.src = 'audios/player_down.wav'
@@ -14,9 +18,11 @@ temaMario.src = 'audios/temaMario.wav'
 
 
 
+
 const jump = () => {
   mario.classList.add('jump');
   jumpHit.play();
+
 
 
 
@@ -36,21 +42,23 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
 
 if (pipePosition <= 37 && pipePosition > 0 && marioPosition < 80){
+  temaMario.pause();
 
-  pipe.style.animation = 'none';
-  pipe.style.left = `${pipePosition}px`;
-
-  mario.style.animation = 'none';
-  mario.style.bottom = `${marioPosition}px`;
-
+  somHit.play();
   mario.src = './img/game-over.png';
   mario.style.marginLeft= '41px';
   mario.style.width = '75px';
   mario.style.animation = 'gameover-animation 3s';
+  mario.style.animation = 'none';
+  mario.style.bottom = `${marioPosition}px`;
+
+  pipe.style.animation = 'none';
+  pipe.style.left = `${pipePosition}px`;
 
 
-  temaMario.pause();
-  somHit.play();
+  fimJogo();
+
+
 
 
 
@@ -59,6 +67,9 @@ if (pipePosition <= 37 && pipePosition > 0 && marioPosition < 80){
   clearInterval(lopp);
 
 }
+  count++;
+  score.innerHTML = `SCORE: ${count}`;
+
 }, 10);
 
 const loppBlock = setInterval(() => {
@@ -72,6 +83,7 @@ if (blockPosition <= 80 && blockPosition > 0 && marioPosition < 80){
 
   block.style.animation = 'none';
   block.style.left = `${blockPosition}px`;
+
 
   mario.style.animation = 'none';
   mario.style.bottom = `${marioPosition}px`;
@@ -92,6 +104,9 @@ if (blockPosition <= 80 && blockPosition > 0 && marioPosition < 80){
   clearInterval(loppBlock);
 
 }
+
+count++;
+score.innerHTML = `SCORE: ${count}`;
 }, 10);
 
 document.addEventListener('keydown', (e) => {
@@ -99,3 +114,20 @@ document.addEventListener('keydown', (e) => {
     jump();
   }
 });
+
+function fimJogo() {
+  const container = document.querySelector('.container');
+  const textGameOver = document.createElement("h1");
+  textGameOver.innerText = `GAME OVER!
+  A sua pontuação é ${count}`;
+
+  textGameOver.style.fontSize = "28px";
+  textGameOver.style.textAlign = "center";
+  textGameOver.style.marginTop = "300px";
+  textGameOver.style.backgroundColor = "black";
+  textGameOver.style.color = "#f3ff12";
+
+
+  gameBoard.appendChild(textGameOver);
+
+};
